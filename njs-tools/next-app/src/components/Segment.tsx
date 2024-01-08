72890e364bd3ccad813819cc4dac2b1cd086c7ab
t3llscode <git@t3l.ls>
2024-01-08 13:05:21 +0000

    V0.2.1 (functions in segment)

--- njs-tools/next-app/src/components/Segment.tsx
+++ njs-tools/next-app/src/components/Segment.tsx
@@ -1,15 +1,32 @@
-import styles from './segment.module.css';
-
-export default function Segment({ radius, stroke, percentage, reversed }) {
+export default function Segment({ radius, stroke, rotation, percentage, reversed, background, startC, endC, doneC }) {
 
   const SIZE = radius + stroke;
   const CIRCUMFERENCE = Math.PI * radius * 8;
 
+  const getColor = (percentage: number) => {
+    const r1 = parseInt(startC.slice(1, 3), 16);
+    const g1 = parseInt(startC.slice(3, 5), 16);
+    const b1 = parseInt(startC.slice(5, 7), 16);
+
+    const r2 = parseInt(endC.slice(1, 3), 16);
+    const g2 = parseInt(endC.slice(3, 5), 16);
+    const b2 = parseInt(endC.slice(5, 7), 16);
+
+    const r = Math.round(r1 + (r2 - r1) * percentage);
+    const g = Math.round(g1 + (g2 - g1) * percentage);
+    const b = Math.round(b1 + (b2 - b1) * percentage);
+
+    return `rgb(${r}, ${g}, ${b})`;
+  };
+
+  const foreground = getColor(percentage);
+
   return (
-    <div className={styles.container}>
-      <svg height={SIZE} width={SIZE} style={reversed ? { transform: 'scaleX(-1) rotate(90deg)' } : {}}>
-        <circle r={radius} stroke="#FFFFFF" strokeWidth={stroke} fill="none" pathLength={CIRCUMFERENCE} strokeDasharray={`${percentage * CIRCUMFERENCE * 0.25} ${CIRCUMFERENCE}`} />
-      </svg>
+    <div>
+      <svg height={SIZE} width={SIZE} style={{transform: `rotate(${rotation}deg)`}}>
+          <circle r={radius} stroke={background} strokeWidth={stroke} fill="transparent" />
+          <circle r={radius} stroke={percentage != 1 ? foreground : doneC} strokeWidth={stroke} fill="transparent" pathLength={CIRCUMFERENCE} strokeDasharray={`${percentage * CIRCUMFERENCE * 0.25} ${CIRCUMFERENCE}`} style={reversed ? { transform: `scaleX(-1) rotate(90deg)` } : {}} />
+      </svg> 
     </div>
   );
 };
--- njs-tools/next-app/src/pages/timer.tsx
+++ njs-tools/next-app/src/pages/timer.tsx
@@ -1,25 +1,97 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import customStyles from './timer.module.css'
 import Segment from '../components/Segment.tsx';
+
+import { useState, useEffect } from 'react';
 
 export default function Home() {
 
-  const percentages = [25, 50, 75, 100];
+  const [config, setConfig] = useState([
+    {
+      name: "outer",
+      styles: {
+        radius: 50,
+        stroke: 3,
+        reversed: false,
+        background: "#222222",
+        foreground: "#FFFFFF",
+        gradient: true,
+        donecolor: "#00FF00"
+      }
+    },
+    {
+      name: "chewing",
+      styles: {
+        radius: 50,
+        stroke: 3,
+        reversed: false,
+        background: "#222222",
+        foreground: "#FFFFFF",
+        gradient: true,
+        donecolor: "#00FF00"
+      }
+    },
+    {
+      name: "inner",
+      styles: {
+        radius: 50,
+        stroke: 3,
+        reversed: false,
+        background: "#222222",
+        foreground: "#FFFFFF",
+        gradient: true,
+        donecolor: "#00FF00"
+      }
+    }
+  ]);
+
+  const [percentages, setPercentages] = useState({
+    outer: {
+      tl: 10,
+      tr: 90,
+      bl: 95,
+      br: 100
+    },
+    chewing: {
+      tl: 10,
+      tr: 90,
+      bl: 95,
+      br: 100
+    },
+    inner: 95
+  });
+
+  const segment = [10, 90, 95, 100]
+  const circle = [0, 0, 0]
+  const rotation = [180, 270, 90, 0]
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next app</title>
+        <title>t3lls tooth time</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className={customStyles.title}>
           Welcome to my Timer
         </h1>
-        <Segment radius={50} stroke={3} percentage={0.5} reversed={false}/>
-        
+
+        <div>
+          
+          { circle.map((circle, index) => (
+            <div className={customStyles.grid2x2}>
+              {segment.map((percentage, index) => (
+                <Segment radius={200} stroke={3} rotation={rotation[index]} percentage={percentage / 100} reversed={false} background={"#222222"} startC={"#AAAAAA"} endC={"#77FF77"} doneC={"#00FF00"}/>
+              ))}
+            </div>
+          ))}
+
+        </div>
+
+
         
       </main>
 
