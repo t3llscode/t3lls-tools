d28dd9334bdd97d1e3b4a12478a5578992025c69
t3llscode <git@t3l.ls>
2024-12-25 23:55:06 +0000

    V1.6 (add certificate creator bare page / modules, not working yet)

---
+++ njs-tools/next-app/src/components/multi purpose/header.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Image from 'next/image' // better than <img> tag
+import styles from './header.module.scss'
+
+import { useState, useEffect, useRef } from 'react';
+
+export default function Header({ title }) {
+
+    return (
+        <div>
+            <div className={styles.header_box}>
+                <div className={styles.box}>
+                    <img src="/data/media/icons/t3lls/t3lls.svg" height="16px"/>
+                    <p className={styles.header_text}>
+                        {title}
+                    </p>
+                </div>
+                <div className={styles.box}>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
--- njs-tools/next-app/src/pages/certificate.tsx
+++ njs-tools/next-app/src/pages/certificate.tsx
@@ -1,23 +1,50 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import customStyles from './timer.module.css'
+import customStyles from './certificate.module.scss'
 
 import { useState, useEffect, useRef } from 'react';
+
+import Header from '../components/multi purpose/header'
 
 export default function Home() {
 
 
     return (
         <div>
-            Test 234
-            {/* <div className={customStyles.timer}>*/}
+            <Head>
+                <title>t3lls certificate creator</title>
+                <link rel="icon" href="/data/media/t3.ico" />
+            </Head>
 
 
-            {/* <div className={customStyles.timer}>*/}
 
+            {/* Header */}
+            
 
+            <img src="/data/media/icons/t3lls/t3lls.svg" height="16px"/>
+            <div>
+                <div>
+                    before
+                </div>
+                <Header title="Certificate Creator"/>
+                <div>
+                    after
+                </div>
+            </div>
 
-            {/* <div className={customStyles.timer}>*/}
+            {/* File Upload or Default Set*/}
+                {/* "Slider Check" for File Upload or Default Set */}
+
+                {/* You are using "upload.csv" / the default set*/}
+
+            {/* Two Columns */}
+                {/* Drop Down Section */}
+
+                {/* Preview */}
+
+            {/* Export */}
+
+            {/* Footer */}
         </div>
     )
 
